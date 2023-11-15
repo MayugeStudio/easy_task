16e09e793ff9dd4dc585d10b2552e9080f74fbf5
Mayuge <mayuge.studio.public@gmail.com>
2023-11-15 23:14:58 +0900

    feat: Implement LineHandler interface to handle line more easy.

--- code/parse_lines.go
+++ code/parse_lines.go
@@ -2,30 +2,23 @@ package code
 
 import "strings"
 
-// ParseTaskStringsToTasks processes a slice of strings representing tasks
-// and returns a slice of Task pointers
 func ParseTaskStringsToTasks(taskStrings []string) []*Task {
 	tasks := make([]*Task, 0)
 	taskStrings = FormatTaskStrings(taskStrings)
-	for _, str := range taskStrings {
-		if strings.HasPrefix(str, "-") {
-			tokens := strings.Fields(str)
-			task := ParseTaskStringToTask(tokens)
-			tasks = append(tasks, task)
-		} else {
-			continue
-		}
+	taskLines := createLineHandlersFromStrings(taskStrings)
+	for _, line := range taskLines {
+		task := ParseTaskLineToTask(line)
+		tasks = append(tasks, task)
 	}
 	return tasks
 }
 
-// ParseTaskStringToTask takes a slice of strings representing tokens
-// and convert them into a Task pointer.
-func ParseTaskStringToTask(tokens []string) *Task {
+func ParseTaskLineToTask(line LineHandler) *Task {
 	task := NewTask()
+	tokens := line.Tokenize()
 	// Process each token until the tokens slice is empty.
 	for len(tokens) > 0 {
-		token := strings.ToUpper(tokens[0])
+		token := tokens[0]
 		switch token {
 		case "X":
 			task.IsDone = true
@@ -38,4 +31,54 @@ func ParseTaskStringToTask(tokens []string) *Task {
 		}
 	}
 	return task
+}
+
+func createLineHandlersFromStrings(rawTaskStrings []string) []LineHandler {
+	result := make([]LineHandler, 0)
+	var lineHandler LineHandler
+	for _, str := range rawTaskStrings {
+		lineHandler = createLineHandlerFromString(str)
+		result = append(result, lineHandler)
+	}
+	return result
+}
+
+func createLineHandlerFromString(rawString string) LineHandler {
+	str := strings.TrimPrefix(rawString, "-")
+	str = strings.TrimSpace(str)
+
+	if strings.HasPrefix(str, "[") {
+		return &taskLine{str: rawString}
+	}
+
+	return &groupLine{str: rawString}
+}
+
+type LineHandler interface {
+	Tokenize() []string
+	String() string
+}
+
+type taskLine struct {
+	str string
+}
+
+func (t *taskLine) Tokenize() []string {
+	panic("implement me")
+}
+
+func (t *taskLine) String() string {
+	return t.str
+}
+
+type groupLine struct {
+	str string
+}
+
+func (g *groupLine) Tokenize() []string {
+	panic("implement me")
+}
+
+func (g *groupLine) String() string {
+	return g.str
 }
