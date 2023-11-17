b47532835f50ff2c793858d44ff8e83bbc0f656f
Mayuge <mayuge.studio.public@gmail.com>
2023-11-18 08:33:48 +0900

    refactor: Inline ParseStringToTask

--- code/parse_strings_to_tasks.go
+++ code/parse_strings_to_tasks.go
@@ -13,30 +13,25 @@ func ParseStringsToTasks(taskStrings []string) []*Task {
 			str = strings.Replace(str, "]", "", 1)
 			str = strings.TrimSpace(str)
 			tokens := strings.Fields(str)
-			task := ParseStringToTask(tokens)
+			task := NewTask()
+			// Process each token until the tokens slice is empty.
+			for len(tokens) > 0 {
+				token := tokens[0]
+				switch token {
+				case "X":
+					task.IsDone = true
+				default:
+					task.Title = strings.Join(tokens, " ")
+					tokens = nil
+				}
+				if len(tokens) > 0 {
+					tokens = tokens[1:]
+				}
+			}
 			tasks = append(tasks, task)
 		} else {
 			continue
 		}
 	}
 	return tasks
-}
-
-func ParseStringToTask(tokens []string) *Task {
-	task := NewTask()
-	// Process each token until the tokens slice is empty.
-	for len(tokens) > 0 {
-		token := strings.ToUpper(tokens[0])
-		switch token {
-		case "X":
-			task.IsDone = true
-		default:
-			task.Title = strings.Join(tokens, " ")
-			tokens = nil
-		}
-		if len(tokens) > 0 {
-			tokens = tokens[1:]
-		}
-	}
-	return task
 }
