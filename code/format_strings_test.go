5f8fe14685dad195d6be7f0b9d9648ed95615bf5
Mayuge <mayuge.studio.public@gmail.com>
2023-11-18 01:51:58 +0900

    refactor: Refactor test

--- code/format_strings_test.go
+++ code/format_strings_test.go
@@ -32,21 +32,43 @@ func TestFormatTaskStrings(t *testing.T) {
 	}
 }
 
-func TestFormatTaskString(t *testing.T) {
+func TestFormatTaskString_Task(t *testing.T) {
+	validStringDone := "- [X] Buy the milk."
+	validStringUndone := "- [ ] Buy the milk."
+	errString := ""
 	tests := map[string]struct {
 		in   string
 		want string
 	}{
-		"TaskLine_Valid":            {"- [ ] Buy the milk.", "- [ ] Buy the milk."},
-		"UndoneTaskLine_BadIndent1": {"-[] Buy the milk.", "- [ ] Buy the milk."},
-		"UndoneTaskLine_BadIndent2": {"- []Buy the milk.", "- [ ] Buy the milk."},
-		"UndoneTaskLine_BadIndent3": {"- []Buy the milk.", "- [ ] Buy the milk."},
-		"DoneTaskLine_BadIndent1":   {"-[X] Buy the milk.", "- [X] Buy the milk."},
-		"DoneTaskLine_BadIndent2":   {"- [X]Buy the milk.", "- [X] Buy the milk."},
-		"DoneTaskLine_BadIndent3":   {"-[X]Buy the milk.", "- [X] Buy the milk."},
-		"NotStartWithDash":          {"[] notStartWithDash.", ""},
-		"GroupLine_Valid":           {"-Buy the milk task group.", "- Buy the milk task group."},
-		"GroupLine_BadIndent":       {"-Buy the milk task group.", "- Buy the milk task group."},
+		"Undone_Valid":                 {"- [ ] Buy the milk.", validStringUndone},
+		"Undone_NoDash":                {"[ ] Buy the milk.", errString},
+		"Undone_BadIndentStartBracket": {"-[] Buy the milk.", validStringUndone},
+		"Undone_BadIndentEndBracket":   {"- []Buy the milk.", validStringUndone},
+		"Done_Valid":                   {"- [X] Buy the milk.", validStringDone},
+		"Done_NoDash":                  {"[X] No Dash.", errString},
+		"Done_NoSpaceInBracket":        {"- [X] Buy the milk.", validStringDone},
+		"Done_BadIndentStartEnd":       {"-[X]Buy the milk.", validStringDone},
+	}
+	for testName, tt := range tests {
+		t.Run(testName, func(t *testing.T) {
+			got := FormatTaskString(tt.in)
+			if got != tt.want {
+				t.Errorf("FormatTaskString() = %q, want %q", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFormatTaskString_Group(t *testing.T) {
+	validString := "- Buy the milk task group."
+	errString := ""
+	tests := map[string]struct {
+		in   string
+		want string
+	}{
+		"Valid":     {"- Buy the milk task group.", validString},
+		"BadIndent": {"-Buy the milk task group.", validString},
+		"NoDash":    {"Buy the milk task group.", errString},
 	}
 	for testName, tt := range tests {
 		t.Run(testName, func(t *testing.T) {
