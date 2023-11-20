bac5f887e09d8e43ab625694b0d551cfa3871a16
Mayuge <mayuge.studio.public@gmail.com>
2023-11-20 16:08:12 +0900

    refactor: Remove redundant if statement.
    I removed the if statement at line 9 because it was always true,
     making it unnecessary and adding unnecessary complexity to the code.
     This change improve code readability and simplifies the logic.

--- code/parse_strings_to_tasks.go
+++ code/parse_strings_to_tasks.go
@@ -6,34 +6,30 @@ func ParseStringsToTasks(taskStrings []string) []*Task {
 	tasks := make([]*Task, 0)
 	taskStrings = FormatTaskStrings(taskStrings)
 	for _, str := range taskStrings {
-		if strings.HasPrefix(str, "-") {
-			str = strings.TrimPrefix(str, "-")
-			str = strings.TrimSpace(str)
-			str = strings.Replace(str, "[", "", 1)
-			str = strings.Replace(str, "]", "", 1)
-			str = strings.TrimSpace(str)
-			tokens := strings.Fields(str)
-			title := ""
-			isDone := false
-			// Process each token until the tokens slice is empty.
-			for len(tokens) > 0 {
-				token := tokens[0]
-				switch token {
-				case "X":
-					isDone = true
-				default:
-					title = strings.Join(tokens, " ")
-					tokens = nil
-				}
-				if len(tokens) > 0 {
-					tokens = tokens[1:]
-				}
+		str = strings.TrimPrefix(str, "-")
+		str = strings.TrimSpace(str)
+		str = strings.Replace(str, "[", "", 1)
+		str = strings.Replace(str, "]", "", 1)
+		str = strings.TrimSpace(str)
+		tokens := strings.Fields(str)
+		title := ""
+		isDone := false
+		// Process each token until the tokens slice is empty.
+		for len(tokens) > 0 {
+			token := tokens[0]
+			switch token {
+			case "X":
+				isDone = true
+			default:
+				title = strings.Join(tokens, " ")
+				tokens = nil
 			}
-			task := NewTask(title, isDone)
-			tasks = append(tasks, task)
-		} else {
-			continue
+			if len(tokens) > 0 {
+				tokens = tokens[1:]
+			}
 		}
+		task := NewTask(title, isDone)
+		tasks = append(tasks, task)
 	}
 	return tasks
 }
