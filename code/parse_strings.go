7674b8245eebac684359a18a9f4b4ba3e3fc79c3
Mayuge <mayuge.studio.public@gmail.com>
2023-11-21 16:17:11 +0900

    feat: Branch the process in ParseStringsToTasks by using if statements.

--- code/parse_strings.go
+++ code/parse_strings.go
@@ -6,30 +6,41 @@ func ParseStringsToTasks(taskStrings []string) *TodoItemContainer {
 	todoItemContainer := NewTodoItemContainer()
 	taskStrings = FormatTaskStrings(taskStrings)
 	for _, str := range taskStrings {
-		str = strings.TrimPrefix(str, "-")
-		str = strings.TrimSpace(str)
-		str = strings.Replace(str, "[", "", 1)
-		str = strings.Replace(str, "]", "", 1)
-		str = strings.TrimSpace(str)
-		tokens := strings.Fields(str)
-		title := ""
-		isDone := false
-		// Process each token until the tokens slice is empty.
-		for len(tokens) > 0 {
-			token := tokens[0]
-			switch token {
-			case "X":
-				isDone = true
-			default:
-				title = strings.Join(tokens, " ")
-				tokens = nil
+		if IsSingleTaskString(str) {
+			str = strings.TrimPrefix(str, "-")
+			str = strings.TrimSpace(str)
+			str = strings.Replace(str, "[", "", 1)
+			str = strings.Replace(str, "]", "", 1)
+			str = strings.TrimSpace(str)
+			tokens := strings.Fields(str)
+			title := ""
+			isDone := false
+			// Process each token until the tokens slice is empty.
+			for len(tokens) > 0 {
+				token := tokens[0]
+				switch token {
+				case "X":
+					isDone = true
+				default:
+					title = strings.Join(tokens, " ")
+					tokens = nil
+				}
+				if len(tokens) > 0 {
+					tokens = tokens[1:]
+				}
 			}
-			if len(tokens) > 0 {
-				tokens = tokens[1:]
-			}
+			task := NewTask(title, isDone)
+			todoItemContainer.AddTask(task)
+			continue
 		}
-		task := NewTask(title, isDone)
-		todoItemContainer.AddTask(task)
+
+		if IsGroupTitle(str) {
+			continue
+		}
+
+		if IsGroupTaskString(str) {
+			continue
+		}
 	}
 	return todoItemContainer
 }
