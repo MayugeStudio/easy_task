77d948d633eb3784e17b09d08b9d0350af7210cb
Mayuge <mayuge.studio.public@gmail.com>
2023-11-22 04:57:32 +0900

    refactor: Integrate 'format_strings.go' and 'format_strings_helpers.go'.

--- src/logic/format_strings.go
+++ src/logic/format_strings.go
@@ -7,6 +7,14 @@ import (
 	"strings"
 )
 
+var (
+	SyntaxError         = errors.New("format error")
+	NoDashError         = fmt.Errorf("%w: no dash", SyntaxError)
+	NoBracketStartError = fmt.Errorf("%w: no bracket start", SyntaxError)
+	NoBracketEndError   = fmt.Errorf("%w: no bracket end", SyntaxError)
+	InvalidIndentError  = fmt.Errorf("%w: no valid indent", SyntaxError)
+)
+
 func FormatTaskStrings(taskStrings []string) []string {
 	result := make([]string, 0)
 	errs := make([]error, 0)
@@ -87,4 +95,95 @@ func FormatTaskString(s string) (string, error) {
 	l = l.TrimPrefix("]").TrimSpace()
 
 	return fmt.Sprintf("- [%s] %s", statusStr, l), nil
+}
+
+func GetStatusString(taskString string) (string, error) {
+	if !strings.HasPrefix(taskString, "-") {
+		return "", NoDashError
+	}
+
+	l := line.New(taskString)
+
+	l = l.TrimPrefix("-").TrimSpace()
+
+	if !l.HasPrefix("[") {
+		return "", NoBracketStartError
+	}
+	l = l.TrimPrefix("[").TrimSpace()
+
+	if l.HasPrefix("X") || l.HasPrefix("x") {
+		return "X", nil
+	}
+	return " ", nil
+}
+
+func GetGroupTitle(s string) (string, error) {
+	if !strings.HasPrefix(s, "-") {
+		return "", fmt.Errorf("%w: invalid group title %q", SyntaxError, s)
+	}
+	s = strings.TrimPrefix(s, "-")
+	return strings.TrimSpace(s), nil
+}
+
+func IsGroupTitle(s string) bool {
+	if !strings.HasPrefix(s, "-") {
+		return false
+	}
+	l := line.New(s)
+	l = l.TrimPrefix("-").TrimSpace()
+	if l.HasPrefix("[") {
+		return false
+	}
+	return true
+}
+
+func IsGroupTaskString(s string) bool {
+	if !strings.HasPrefix(s, " ") {
+		return false
+	}
+	l := line.New(strings.TrimSpace(s))
+
+	if !l.HasPrefix("-") {
+		return false
+	}
+	l = l.TrimPrefix("-").TrimSpace()
+
+	if !l.HasPrefix("[") {
+		return false
+	}
+	l = l.TrimPrefix("[").TrimSpace()
+
+	if l.HasPrefix("X") || l.HasPrefix("x") {
+		l = l.TrimPrefix("X").TrimSpace()
+	}
+
+	if !l.HasPrefix("]") {
+		return false
+	}
+
+	return true
+}
+
+func IsSingleTaskString(s string) bool {
+	if !strings.HasPrefix(s, "-") {
+		return false
+	}
+
+	l := line.New(s)
+	l = l.TrimPrefix("-").TrimSpace()
+
+	if !l.HasPrefix("[") {
+		return false
+	}
+	l = l.TrimPrefix("[").TrimSpace()
+
+	if l.HasPrefix("X") || l.HasPrefix("x") {
+		l = l.TrimPrefix("X").TrimSpace()
+	}
+
+	if !l.HasPrefix("]") {
+		return false
+	}
+
+	return true
 }
