8a47974e360eb3ca52386ca7dd51ae6301e29c7a
Mayuge <mayuge.studio.public@gmail.com>
2023-11-26 05:06:26 +0900

    feat: Implement TodoList struct to ProgressItem interface.

--- domain/todo_list.go
+++ domain/todo_list.go
@@ -3,6 +3,7 @@ package domain
 type TodoList struct {
 	tasks         []*Task
 	groups        []*Group
+	items         []ProgressItem
 	doneTaskCount int
 }
 
@@ -10,6 +11,7 @@ func NewTodoList() *TodoList {
 	return &TodoList{
 		tasks:         make([]*Task, 0),
 		groups:        make([]*Group, 0),
+		items:         make([]ProgressItem, 0),
 		doneTaskCount: 0,
 	}
 }
@@ -19,10 +21,12 @@ func (c *TodoList) AddTask(t *Task) {
 	if t.IsDone {
 		c.doneTaskCount++
 	}
+	c.items = append(c.items, t)
 }
 
 func (c *TodoList) AddGroup(g *Group) {
 	c.groups = append(c.groups, g)
+	c.items = append(c.items, g)
 }
 
 func (c *TodoList) GetTasks() []*Task {
@@ -31,4 +35,18 @@ func (c *TodoList) GetTasks() []*Task {
 
 func (c *TodoList) GetGroups() []*Group {
 	return c.groups
+}
+
+func (c *TodoList) Progress() float64 {
+	if len(c.items) == 0 {
+		return 0
+	}
+
+	var sum float64
+	totalNum := len(c.items)
+	for _, item := range c.items {
+		sum += item.Progress()
+	}
+
+	return sum / float64(totalNum)
 }
