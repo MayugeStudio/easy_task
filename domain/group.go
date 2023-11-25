5f04e31efec0503056ba319cb217b5aca84f6a43
Mayuge <mayuge.studio.public@gmail.com>
2023-11-26 05:06:15 +0900

    feat: Implement Group struct to ProgressItem interface.

--- domain/group.go
+++ domain/group.go
@@ -15,3 +15,14 @@ func NewGroup(title string) *Group {
 func (g *Group) AddTask(t *Task) {
 	g.Tasks = append(g.Tasks, t)
 }
+
+func (g *Group) Progress() float64 {
+	if len(g.Tasks) == 0 {
+		return 0
+	}
+	var sum float64
+	for _, task := range g.Tasks {
+		sum += task.Progress()
+	}
+	return sum / float64(len(g.Tasks))
+}
