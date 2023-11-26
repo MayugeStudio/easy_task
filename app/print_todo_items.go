f940a35435b867f9496b3fde6708b6c7b0f072cc
Mayuge <mayuge.studio.public@gmail.com>
2023-11-26 11:43:36 +0900

    refactor: Change variable names.

--- app/print_todo_items.go
+++ app/print_todo_items.go
@@ -14,14 +14,14 @@ func PrintTodoItem(w io.Writer, fileName string, reader logic.FileReader) error 
 	if scanErr != nil {
 		return fmt.Errorf("scanning file: %w", scanErr)
 	}
-	todoItemContainer := parse.ToTodoList(lines)
-	if err := print.Tasks(w, todoItemContainer.GetTasks()); err != nil {
+	todoList := parse.ToTodoList(lines)
+	if err := print.Tasks(w, todoList.GetTasks()); err != nil {
 		return fmt.Errorf("printing tasks: %w", err)
 	}
-	if err := print.Groups(w, todoItemContainer.GetGroups()); err != nil {
+	if err := print.Groups(w, todoList.GetGroups()); err != nil {
 		return fmt.Errorf("printing groups: %w", err)
 	}
-	if err := print.Progress(w, todoItemContainer); err != nil {
+	if err := print.Progress(w, todoList); err != nil {
 		return fmt.Errorf("printing progress: %w", err)
 	}
 	return nil
